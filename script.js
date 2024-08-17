5b9353171f4bb497ffe1ea960ed1408dde354bcc
aakashdeep2310 <rjaakashdeep007@gmail.com>
2024-08-17 18:13:24 +0530

    updated

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const API_KEY = "dd57c36c53d3449dabc85eaa1ccf6f12";
-const url = "https://newsapi.org/v2/everything?q=";
-
-window.addEventListener('load', () => fetchNews("India"))
+window.addEventListener('load', () => fetchNews("India"));
 
 function reload() {
     window.location.reload();
@@ -9,7 +6,7 @@ function reload() {
 
 async function fetchNews(query) {
     try {
-        const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+        const res = await fetch(`/.netlify/functions/fetchNews?query=${query}`);
         if (!res.ok) throw new Error('Network response was not ok.');
         const data = await res.json();
         bindData(data.articles);
@@ -18,24 +15,24 @@ async function fetchNews(query) {
     }
 }
 
-function bindData(articles){
+function bindData(articles) {
     const cardContainer = document.getElementById('cards-container');
     const newsCardTemplate = document.getElementById('template-news-card');
 
-    cardContainer.innerHTML = " ";
+    cardContainer.innerHTML = "";
 
     articles.forEach((article) => {
-        if(!article.urlToImage) return;
-        const cardClone = newsCardTemplate.content.cloneNode(true); // deep cloning => all div will be clone recursively
+        if (!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
         fillDataInCard(cardClone, article);
         cardContainer.appendChild(cardClone);
     });
 }
 
-function fillDataInCard(cardClone, article){
+function fillDataInCard(cardClone, article) {
     const newsImage = cardClone.querySelector('#news-img');
     const newsTitle = cardClone.querySelector('#news-title');
-    const newsSource= cardClone.querySelector('#news-source');
+    const newsSource = cardClone.querySelector('#news-source');
     const newsDesc = cardClone.querySelector('#news-desc');
 
     newsImage.src = article.urlToImage;
@@ -53,28 +50,23 @@ function fillDataInCard(cardClone, article){
     });
 }
 
-
 let currSelectedNav = null;
 
-
-function onNavItemClick(id){
+function onNavItemClick(id) {
     fetchNews(id);
-    const navItem =document.getElementById(id);
+    const navItem = document.getElementById(id);
     currSelectedNav?.classList.remove('active');
     currSelectedNav = navItem;
     currSelectedNav.classList.add('active');
 }
 
 const searchButton = document.getElementById('search-button');
-const searchText= document.getElementById('search-text');
-
+const searchText = document.getElementById('search-text');
 
 searchButton.addEventListener('click', () => {
     const query = searchText.value;
-    if(!query) return;
+    if (!query) return;
     fetchNews(query);
     currSelectedNav?.classList.remove('active');
     currSelectedNav = null;
 });
-
-
